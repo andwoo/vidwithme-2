f5782d54500ee8de508659454650e080742089ed
Andrew M <andrewlmanley@gmail.com>
2020-03-22 11:15:46 -0400

    Added generic overlay

--- client/src/components/ErrorOverlay.tsx
+++ client/src/components/ErrorOverlay.tsx
@@ -1,32 +1,24 @@
 import * as React from 'react';
 import * as StoreModels from '../redux/interfaces/StoreModels';
+import GenericOverlay from './GenericOverlay';
 
 interface ErrorOverlayProps extends StoreModels.RoomError {
   onClose: () => void;
 }
 
 export default class ErrorOverlay extends React.Component<ErrorOverlayProps> {
-  constructor(props) {
-    super(props);
-    console.dir(props);
-  }
   handleOnClose = () => {
     this.props.onClose && this.props.onClose();
   }
   render() {
     return (
-      <div className="modal" style={{display: "inline-flex"}}>
-        <div className="modal-background"></div>
-        <article className="modal-card message is-danger">
-          <div className="message-header">
-            <p>{this.props.title}</p>
-            <button className="delete" aria-label="delete" onClick={this.handleOnClose}></button>
-          </div>
-          <div className="message-body">
-            {this.props.description}
-          </div>
-        </article>
-      </div>
+      <GenericOverlay
+        overlayType="danger"
+        title={this.props.title}
+        showCloseButton={true}
+        onClose={this.handleOnClose}>
+          {this.props.description}
+      </GenericOverlay>
     );
   }
 }
---
+++ client/src/components/GenericOverlay.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+
+interface GenericOverlayProps {
+  overlayType: string; //dark, primary, link, info, success, warning, danger, 
+  title: string;
+  showCloseButton: boolean;
+  onClose: () => void;
+}
+
+export default class GenericOverlay extends React.Component<GenericOverlayProps> {
+  constructor(props) {
+    super(props);
+    console.dir(props);
+  }
+  handleOnClose = () => {
+    this.props.onClose && this.props.onClose();
+  }
+  render() {
+    return (
+      <div className="modal" style={{display: "inline-flex"}}>
+        <div className="modal-background"></div>
+        <article className={`modal-card message is-${this.props.overlayType}`}>
+          <div className="message-header">
+            <p>{this.props.title}</p>
+            {this.props.showCloseButton && <button className="delete" aria-label="delete" onClick={this.handleOnClose}/>}
+          </div>
+          <div className="message-body">
+            {this.props.children}
+          </div>
+        </article>
+      </div>
+    );
+  }
+}
