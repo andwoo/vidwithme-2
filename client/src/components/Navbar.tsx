468b306c3263c5d551769af3bf3181a0e2fafb8e
Andrew M <andrewlmanley@gmail.com>
2020-03-28 19:36:50 -0400

    Added room url copy to clipboard logic

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,6 +2,27 @@ import * as React from 'react';
 import { Store } from '../redux/Store';
 
 export default class Navbar extends React.Component<Store> {
+
+  isInRoom = () : boolean => {
+    return this.props.room.id && this.props.room.id.length > 0;
+  }
+  isCopySupported = () : boolean => {
+    return this.isInRoom() && !!navigator.clipboard;
+  }
+
+  handleCopyRoomUrl = () => {
+    navigator.clipboard.writeText(`${location.protocol}//${location.host}/${this.props.room.id}`);
+  }
+
+  renderClipboard = () => {
+    if(this.isCopySupported()) {
+      return (
+        <i onClick={this.handleCopyRoomUrl} className="far fa-clipboard clipboard"/>
+      );
+    }
+    return null;
+  }
+
   render () {
     return (
       <div className="columns is-gapless is-mobile has-background-white navbar">
@@ -14,6 +35,7 @@ export default class Navbar extends React.Component<Store> {
         </div>
         <div className="column has-text-right navbar--right">
           <span className="is-size-4 is-family-monospace has-text-danger roomId">{this.props.room.id}</span>
+          {this.renderClipboard()}
         </div>
       </div>
     );
