d25466bd417a1d0411e2a4000a383263a521162e
Andrew M <andrewlmanley@gmail.com>
2020-03-28 14:03:42 -0400

    Added site name

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -5,10 +5,14 @@ export default class Navbar extends React.Component<Store> {
   render () {
     return (
       <div className="columns is-gapless is-mobile has-background-white navbar">
-        <div className="column is-half has-text-left navbar--left">
+        <div className="column is-narrow has-text-left navbar--left">
           <img src="/images/logo.svg" className="logo" />
         </div>
-        <div className="column is-half has-text-right navbar--right">
+        <div className="column has-text-left navbar--center">
+          <span className="vid--text">Vid</span>
+          <span className="withme--text">With.me</span>
+        </div>
+        <div className="column has-text-right navbar--right">
           <span className="is-size-4 is-family-monospace has-text-danger roomId">{this.props.room.id}</span>
         </div>
       </div>
